64ad2ec02d85e2ebac6fe03f8f40949f92679fb5
Arnab Bindu Chakraborty <arnabbinduc2005@gmail.com>
2024-10-25 19:58:56 +0530

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,43 @@
+// first page contacts
+
+document.getElementById('linkedin').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/arnab-bindu-chakraborty-8100a0322', '_blank');
+});
+
+document.getElementById('x').addEventListener('click', function() {
+    window.open('https://x.com/ArnabBindu', '_blank');
+});
+
+
+// document.getElementById('email').addEventListener('click', function() {
+//     window.location.href = 'mailto:arnabbinduc@gmail.com';
+// });
+
+
+// document.getElementById('tel').addEventListener('click', function() {
+//     window.location.href = 'tel:+91 9330905205';
+// }); 
+
+
+
+// links for contact adress page
+document.getElementById('email2').addEventListener('click', function() {
+    window.location.href = 'mailto:arnabbinduc@gmail.com';
+});
+
+
+document.getElementById('tel2').addEventListener('click', function() {
+    window.location.href = 'tel:+91 9330905205';
+}); 
+
+document.getElementById('linkedin2').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/arnab-bindu-chakraborty-8100a0322', '_blank');
+});
+
+document.getElementById('github2').addEventListener('click', function() {
+    window.open('https://github.com/Arnab-UEM19', '_blank');
+});
+
+document.getElementById('x2').addEventListener('click', function() {
+    window.open('https://x.com/ArnabBindu', '_blank');
+});
---
+++ script.js
@@ -0,0 +1,45 @@
+document.getElementById('education-dev').addEventListener('click', function() {
+    document.getElementById('education').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
+document.getElementById('events-dev').addEventListener('click', function() {
+    document.getElementById('eventsAttended').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
+document.getElementById('experience-dev').addEventListener('click', function() {
+    document.getElementById('experience').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
+document.getElementById('membership-dev').addEventListener('click', function() {
+    document.getElementById('membership').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('courses-dev').addEventListener('click', function() {
+    document.getElementById('Courses').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('contact-dev').addEventListener('click', function() {
+    document.getElementById('contacts').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
+document.getElementById('iconup').addEventListener('click', function() {
+    document.getElementById('page1').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
